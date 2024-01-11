96069ffb8449787b6ca3601d99bc0805fddaa29b
ayan munshi <ayanmunshi@ayans-MacBook-Air.local>
2024-01-12 02:05:30 +0530

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,16 @@ import { MdDonutLarge } from "react-icons/md";
 import { MdMessage } from "react-icons/md";
 import { CiMenuKebab } from "react-icons/ci";
 import { Avatar, IconButton } from "@mui/material";
+import Sidebar_chats from "./Sidebar_chats";
 
 function Sidebar() {
   return (
-    <div id="total sidebar" style={{ flex: "0.35" }} className=" flex flex-col ">    {/**flex:0.35 actual wp uses */}
+    <div
+      id="total sidebar"
+      style={{ flex: "0.35" }}  /**flex:0.35 actual wp uses */
+      className=" flex flex-col "
+    >
+    
       <div
         id="side header"
         className="flex justify-between p-2 border-r-[1px] border-black"
@@ -25,13 +31,25 @@ function Sidebar() {
           </IconButton>
         </div>
       </div>
-      <div id="side searchbar" className="flex bg-zinc-300 items-center h-[39px] p-[25px]">
-        <div className="flex items-center rounded-xl bg-zinc-200 my-7 px-2">
-             <GoSearch /> 
-             <input type="text" placeholder="Enter here...." className="px-4 rounded-xl m-2 border-none"/></div>
-       
+      <div
+        id="side searchbar"
+        className="flex bg-zinc-300 items-center h-[39px] p-[25px]"
+      >
+        <div className="flex items-center rounded-xl bg-zinc-200  px-7">
+          <GoSearch />
+          <input
+            type="text"
+            placeholder="Enter here...."
+            className="px-7 rounded-xl m-2 border-none"
+          />
         </div>
-      <div id="side chats" className=""> <p>jhf</p> </div>
+      </div>
+      <div id="side chats" className=" flex-1">
+        <Sidebar_chats createnewchat />
+        <Sidebar_chats />
+        <Sidebar_chats />
+        <Sidebar_chats />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar_chats.jsx
@@ -0,0 +1,34 @@
+import { Avatar } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+function Sidebar_chats({ createnewchat }) {
+  const [seedval, setseedval] = useState("");
+
+  useEffect(() => {
+    setseedval(
+      Math.floor(Math.random() * 300) // this calculation is  for random avatar 
+    );
+  }, []);
+
+  const add_new_chat = () => {
+    const chatroomname = prompt("Enter new chat room name");
+  };
+
+  return !createnewchat ? (
+    <div className="flex p-2 cursor-pointer  border-gray-400 border-b-[0.2px] hover:bg-gray-300">
+      <Avatar src={`https://api.dicebear.com/7.x/bottts/svg?seed=${seedval}`} />     {/* ...link of random avatar */}
+      <div id="room name and info" className="m-2">
+        <h2>room name</h2>
+        <p className="text-[10px] ">information....</p>
+      </div>
+    </div>
+  ) : (
+    <div onClick={add_new_chat}>
+      <h2 className=" font-bold p-4  cursor-pointer  border-gray-400 border-b-[0.2px] hover:bg-gray-300">
+        + Add New chat
+      </h2>
+    </div>
+  );
+}
+
+export default Sidebar_chats;
